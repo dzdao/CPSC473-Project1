4533d4fe3a51aa8f82956a37e6d537c1110d6ed2
ashishmerani <ashishmerani@gmail.com>
2016-03-28 19:03:42 -0700

    Added post functionality

--- js/tm_js.js
+++ js/tm_js.js
@@ -5,18 +5,33 @@ showlatestposts();
 
 
 function showlatestposts(){
-    console.log("in latest posts");
     $.get("/readpost", function(data){
             var tm_posts = JSON.parse(data);
 			 for(i=0;i<tm_posts['questions'].length;i++)
 			{
-                var listitem = "<li class='list-group-item'><h4>" +tm_posts['questions'][i]['title']+"</h4><em><a href=" +tm_posts['questions'][i]['link'] +">" +tm_posts['questions'][i]['link']+"</a></em></li>";
+                var listitem = "<li class='list-group-item'><div class='row'><div class='col-xs-12 col-sm-2'><span class='glyphicon glyphicon-chevron-up fa-2x'></span>&nbsp;<span id='q" + tm_posts['questions'][i]['id']+ "'>" + tm_posts['questions'][i]['vote'] + "</span>&nbsp;<span class='glyphicon glyphicon-chevron-down fa-2x'></div><div class='col-xs-12 col-sm-8'><h4>" +tm_posts['questions'][i]['title']+"</h4><em><a href=" +tm_posts['questions'][i]['link'] +">" +tm_posts['questions'][i]['link']+"</a></em></div></div></li>";
 				console.log(listitem);
 				$(".tm_displayposts").append(listitem);
 			}         
 		 });
 }
 
+$("#tm_post").on('click', function(){
+    var title;
+    var link;
+     console.log("before post");
+    title = $("#tm_title").val().trim();
+    link = $("#tm_link").val().trim();
+    console.log("before post1");
+    $.post("/addpost", {title:title,link:link}, function(data){  
+        console.log("after post");
+        $("#tm_title").val("");
+	       $("#tm_link").val("");
+        $(".tm_displayposts").empty();
+        showlatestposts();
+          });
+    
+		});
 
 
 $(document).ready(main);
--- server.js
+++ server.js
@@ -3,17 +3,45 @@ var bodyParser = require("body-parser");
 var fs = require('fs');
 var app = express();
 app.use(express.static(__dirname + '/'));
+
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
-
 
 //readpost
 app.get('/readpost', function(req, res) {
     fs.readFile('db/db.json', 'utf8', function(err, data) {
         console.log(data);
         res.end(data);
+    });
+    
+});
+
+app.post('/addpost', function(req, res) {
+    console.log("in server");
+    var title = req.body.title;
+    var link = req.body.link;
+    var length;
+    var data;
+    var content;
+    fs.readFile('db/db.json', 'utf8', function(err, data){
+        content = JSON.parse(data);
+        length = content['questions'].length;
+        var data = content['questions'];
+
+        data.push({
+            "id" : length + 1,
+            "title": title,
+            "link":link,
+            "vote": 0,
+            "users": []
+        });
+
+        console.log(data);
+        fs.writeFile("db/db.json", JSON.stringify(content), 'utf8');
+        res.end();
     });
     
 });
