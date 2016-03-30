21ded63f63dbcb6796d602f6a8cc9f5500e7da9c
ashishmerani <ashishmerani@gmail.com>
2016-03-29 17:37:33 -0700

    Implemented Login Functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var stormpath = require("express-stormpath");
 var fs = require('fs');
 var app = express();
 app.use(express.static(__dirname + '/'));
@@ -10,16 +11,52 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+
+app.use(stormpath.init(app, {
+  client: {
+    apiKey: {
+      id: '6G6UCTOJJWLBRUZ73PRX023I7',
+      secret: 'i9AIHgjlVBToIhYZX/DUA8fKq+TUgpDEGleQeK6FrgY'
+    }
+  },
+  application: {
+    href: 'https://api.stormpath.com/v1/applications/3UXGpJ9k0hoGYhxR7SKLIv'
+  },
+
+  web: {
+    register: {
+      // after registering and logging in, the user will be redirected to /index.html
+      nextUri: '/index.html'
+      
+    }
+  }
+}));
+app.on('stormpath.ready', function() {
+console.log('Stormpath Ready');
+});
+
+
+
+app.get('/checklogin',stormpath.getUser, function(req, res) {
+    if (req.user) {
+        res.send(req.user.email);
+    } else {
+    res.send('Not logged in');
+    }    
+});
+
+
+
 //readpost
 app.get('/readpost', function(req, res) {
+   
     fs.readFile('db/db.json', 'utf8', function(err, data) {
-        console.log(data);
         res.end(data);
     });
     
 });
 
-app.post('/addpost', function(req, res) {
+app.post('/addpost', stormpath.loginRequired, function(req, res) {
     console.log("in server");
     var title = req.body.title;
     var link = req.body.link;
